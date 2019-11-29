e6bcaa8b23278dec642787166775fa68597e1fc0
passion-logan <wangdadamailbox@163.com>
2019-11-29 18:28:07 +0800

    添加server端代码

--- config-server-git/src/main/java/com/cody/ConfigServerGitApplication.java
+++ config-server-git/src/main/java/com/cody/ConfigServerGitApplication.java
@@ -2,7 +2,12 @@ package com.cody;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * @EnableConfigServer 开启SpringCloud Config服务端功能
+ */
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerGitApplication {
 
