a69e324460a26cbbe90d0e29b4f291d7b7a3f6b9
passion-logan <wangdadamailbox@163.com>
2019-11-29 18:50:50 +0800

    添加注释

--- config-client/src/main/java/com/cody/ConfigClientApplication.java
+++ config-client/src/main/java/com/cody/ConfigClientApplication.java
@@ -3,6 +3,15 @@ package com.cody;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * spring.application.name：对应配置文件规则中的{application}部分
+ * spring.cloud.config.profile：对应配置文件规则中的{profile}部分
+ * spring.cloud.config.label：对应配置文件规则中的{label}部分
+ * spring.cloud.config.uri：配置中心config-server的地址
+ * <p>
+ * 这里需要格外注意：上面这些属性必须配置在bootstrap.properties中，这样config-server中的配置信息才能被正确加载。
+ * 访问http://localhost:2001/info ，我们可以看到该端点将会返回从git仓库中获取的配置信息
+ */
 @SpringBootApplication
 public class ConfigClientApplication {
 
--- config-server-git/src/main/java/com/cody/ConfigServerGitApplication.java
+++ config-server-git/src/main/java/com/cody/ConfigServerGitApplication.java
@@ -6,6 +6,15 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @EnableConfigServer 开启SpringCloud Config服务端功能
+ * <p>
+ * /{application}/{profile}[/{label}]
+ * /{application}-{profile}.yml
+ * /{label}/{application}-{profile}.yml
+ * /{application}-{profile}.properties
+ * /{label}/{application}-{profile}.properties
+ * 上面的url会映射{application}-{profile}.properties对应的配置文件，其中{label}对应Git上不同的分支，默认为master。
+ * 我们可以尝试构造不同的url来访问不同的配置内容，比如，要访问master分支，config-client应用的dev环境，
+ * 就可以访问这个url：http://localhost:8011/config-client/dev/master
  */
 @EnableConfigServer
 @SpringBootApplication
