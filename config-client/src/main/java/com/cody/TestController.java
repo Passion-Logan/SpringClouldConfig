aa5df29f6389c40df9c3612500ee52e623bd4b62
passion-logan <wangdadamailbox@163.com>
2019-12-03 18:39:54 +0800

    修改配置

--- config-client/src/main/java/com/cody/TestController.java
+++ config-client/src/main/java/com/cody/TestController.java
@@ -17,11 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @Value("${from}")
+    /*@Value("${from}")
     private String from;
 
     @RequestMapping("/from")
     public String from() {
         return this.from;
-    }
+    }*/
 }
