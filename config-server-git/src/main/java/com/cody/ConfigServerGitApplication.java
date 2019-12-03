d9f1244a0bf9cb296b5231c4cf6b6540393fb885
passion-logan <wangdadamailbox@163.com>
2019-12-03 18:25:32 +0800

    修改服务中心url

--- config-server-git/src/main/java/com/cody/ConfigServerGitApplication.java
+++ config-server-git/src/main/java/com/cody/ConfigServerGitApplication.java
@@ -2,6 +2,7 @@ package com.cody;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -15,7 +16,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * 上面的url会映射{application}-{profile}.properties对应的配置文件，其中{label}对应Git上不同的分支，默认为master。
  * 我们可以尝试构造不同的url来访问不同的配置内容，比如，要访问master分支，config-client应用的dev环境，
  * 就可以访问这个url：http://localhost:8011/config-client/dev/master
+ *
+ * 通过 @EnableDiscoveryClient 将config-server注册到上面配置的服务注册中心上去
  */
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerGitApplication {
